068c4464d21976ff3dedaeb1115ec21635e752dd
Novii05 <khushiguptask20059@gmail.com>
2025-09-22 22:30:18 +0530

    eval_postfix.c

---
+++ eval_postfix.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
+
+int eval_postfix(const char *exp){
+    int n=strlen(exp);
+    int *stack = malloc(n*sizeof(int));
+    int top=-1;
+    for(int i=0;i<n;i++){
+        char c=exp[i];
+        if(isspace((unsigned char)c)) continue;
+        if(isdigit((unsigned char)c)) stack[++top]=c-'0';
+        else {
+            int b=stack[top--];
+            int a=stack[top--];
+            int res=0;
+            if(c=='+') res=a+b;
+            else if(c=='-') res=a-b;
+            else if(c=='*') res=a*b;
+            else if(c=='/') res=a/b;
+            else if(c=='^'){ int r=1; for(int j=0;j<b;j++) r*=a; res=r; }
+            stack[++top]=res;
+        }
+    }
+    int ans = stack[top];
+    free(stack);
+    return ans;
+}
+
+int main(){
+    char expr[256];
+    printf("Enter postfix expression (single-digit operands): ");
+    if(!fgets(expr,sizeof(expr),stdin)) return 0;
+    expr[strcspn(expr,"\n")] = 0;
+    int res = eval_postfix(expr);
+    printf("Result = %d\n", res);
+    return 0;
+}
